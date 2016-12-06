d71295ef53f4a953d61a328acdca993b1ea71511
Sun ting <suntingwork@126.com>
2016-12-06 11:28:44 +0800

    定义懒加载访客视图控件 extension 和懒加载类型的细节

--- STTWeiBo/Classes/View(视图和控制器)/Main/VisitorView/WBVisitorView.swift
+++ STTWeiBo/Classes/View(视图和控制器)/Main/VisitorView/WBVisitorView.swift
@@ -22,6 +22,23 @@ class WBVisitorView: UIView {
         fatalError("init(coder:) has not been implemented")
     }
 
+    //MARK:私有控件
+    ///懒加载属性只有调用 UIKit 控件的指定函数 其他都需要使用类型
+    /// 图像视图
+    private lazy var iconView:UIImageView = UIImageView(image: UIImage(named: "visitordiscover_feed_image_smallicon"))
+    
+    /// 小房子
+    private lazy var houseIconView:UIImageView = UIImageView(image: UIImage(named: "visitordiscover_feed_image_house"))
+    
+    /// 提示标签
+    private lazy var tipLabel:UILabel = UILabel.cz_labelWithText("关注一些人，回这里看看有什么惊喜,关注一些人，回这里看看有什么惊喜", fontSize: 14, color: UIColor.darkGrayColor())
+    
+    /// 注册按钮
+    private lazy var registerButton:UIButton = UIButton.cz_textButton("注册", fontSize: 16, normalColor: UIColor.orangeColor(), highlightedColor: UIColor.blackColor(), backgroundImageName: "common_button_white_disable")
+    
+    /// 登录按钮
+    private lazy var loginButton:UIButton = UIButton.cz_textButton("登录", fontSize: 16, normalColor: UIColor.darkGrayColor(), highlightedColor: UIColor.blackColor(), backgroundImageName: "common_button_white_disable")
+    
 }
 //MARK: 设置界面
 extension WBVisitorView{
