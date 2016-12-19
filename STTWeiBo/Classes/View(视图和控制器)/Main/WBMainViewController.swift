f6746585a2eaf01144abb01a4822a4c64a26f90c
WeAreSunshineGirl <1391923221@qq.com>
2016-12-19 16:53:36 +0800

    Update WBMainViewController.swift

--- STTWeiBo/Classes/View(视图和控制器)/Main/WBMainViewController.swift
+++ STTWeiBo/Classes/View(视图和控制器)/Main/WBMainViewController.swift
@@ -24,7 +24,7 @@ class WBMainViewController: UITabBarController {
         setupComposeButton()
         setupTimer()
         
-//        setupNewFeatureViews()
+       setupNewFeatureViews()
         
         
         //设置代理
