8460627367375f9fc187a2d704fc047542e940ca
Sun ting <suntingwork@126.com>
2016-12-20 15:12:59 +0800

    微博 cell 的连线

--- STTWeiBo/Classes/View(视图和控制器)/Home/StatusCell/WBStatusCell.swift
+++ STTWeiBo/Classes/View(视图和控制器)/Home/StatusCell/WBStatusCell.swift
@@ -10,6 +10,22 @@ import UIKit
 
 class WBStatusCell: UITableViewCell {
 
+    /// 头像
+    @IBOutlet weak var iconView: UIImageView!
+    /// 姓名
+    @IBOutlet weak var nameLabel: UILabel!
+    /// 会员图标
+    @IBOutlet weak var memberIconView: UIImageView!
+    /// 时间
+    @IBOutlet weak var timeLabel: UILabel!
+   /// 来源
+    @IBOutlet weak var sourceLabel: UILabel!
+       /// 认证图标
+    @IBOutlet weak var vipIconView: UIImageView!
+    /// 微博正文
+    @IBOutlet weak var statusLabel: UILabel!
+    
+    
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
