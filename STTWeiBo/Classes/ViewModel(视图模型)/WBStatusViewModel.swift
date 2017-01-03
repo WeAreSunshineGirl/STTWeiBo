d311e77269cdfeb5b8c6f0113d7234e56bf5482b
Sun ting <suntingwork@126.com>
2017-01-03 10:59:59 +0800

    图片过宽 过窄处理

--- STTWeiBo/Classes/ViewModel(视图模型)/WBStatusViewModel.swift
+++ STTWeiBo/Classes/ViewModel(视图模型)/WBStatusViewModel.swift
@@ -158,12 +158,34 @@ class WBStatusViewModel:CustomStringConvertible {
     
     /**
      使用单个图像 更新配图视图的大小
-     
+     - 新浪针对单张图片 都是缩略图 但是偶尔会有一张特别大的图 7000*9000 多
+     - 新浪微博 为了鼓励原创 支持长微博 但是有的时候 有特别长的微博 长到宽度只有一个点
      - parameter image: 网络缓存的单张图像
      */
     func updateSingleImageSize(image:UIImage){
         
         var size = image.size
+        
+        //过宽图像处理
+        let maxWidth:CGFloat = 300
+        let minWidth:CGFloat = 40
+
+        
+        if size.width > maxWidth {
+            //设置最大宽度
+            size.width = maxWidth
+            
+            //等比例调整高度
+            size.height = size.width * image.size.height / image.size.width
+        }
+        
+        //过窄图像处理
+        if size.width < minWidth {
+        
+            size.width = minWidth
+            //要特殊处理高度 否则高度太大 会影响用户体验
+            size.height = size.width * image.size.height / image.size.width / 4
+        }
         
         // 注意 尺寸需要增加顶部的12个点 便于布局
         size.height += WBStatusPictureViewOutterMargin
