94da360e9d2386bc2082603ad1c66ec1d71dee8e
Sun ting <suntingwork@126.com>
2017-01-05 17:38:34 +0800

    房子和地球设置

--- STTWeiBo/Classes/Tools(工具)/STRefreshControl/STMeiTuanRefreshView.swift
+++ STTWeiBo/Classes/Tools(工具)/STRefreshControl/STMeiTuanRefreshView.swift
@@ -16,4 +16,21 @@ class STMeiTuanRefreshView: STRefreshView {
     @IBOutlet weak var earthIconView: UIImageView!
     
     @IBOutlet weak var kangarooIconView: UIImageView!
+    
+    
+    override func awakeFromNib() {
+        //1 设置房子
+        let bImage1 = UIImage(named: "fangzi")
+        let bImage2 = UIImage(named: "fangzi")
+        
+        bulidingIconView.image = UIImage.animatedImageWithImages([bImage1!,bImage2!], duration: 0.5)
+        // 2设置地球
+        let anim = CABasicAnimation(keyPath: "transform.rotation")
+        anim.toValue = -2 * M_PI
+        anim.repeatCount = MAXFLOAT
+        anim.duration = 5
+        anim.removedOnCompletion = false
+        
+        earthIconView.layer.addAnimation(anim, forKey: nil)
+    }
 }
