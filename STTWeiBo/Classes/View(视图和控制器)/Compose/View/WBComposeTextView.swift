eaf7ee7c20d7c82133b95f3f5d89394c147e935d
suntingting <suntt@wafa.com.cn>
2017-03-31 16:04:29 +0800

    键盘的显示和隐藏处理  以及撰写微博文本视图

---
+++ STTWeiBo/Classes/View(视图和控制器)/Compose/View/WBComposeTextView.swift
@@ -0,0 +1,15 @@
+//
+//  WBComposeTextView.swift
+//  STTWeiBo
+//
+//  Created by user on 17/3/31.
+//  Copyright © 2017年 user. All rights reserved.
+//
+
+import UIKit
+
+/// 撰写微博的文本视图
+class WBComposeTextView: UITextView {
+
+  
+}
