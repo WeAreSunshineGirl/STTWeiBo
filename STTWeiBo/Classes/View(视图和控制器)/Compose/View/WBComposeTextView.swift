c07d0b091922d04a0e01530d54724bbf7af3ae21
suntingting <suntt@wafa.com.cn>
2017-03-31 16:36:43 +0800

    添加占位文本标签以及利用通知监听占位文本的显示和隐藏

--- STTWeiBo/Classes/View(视图和控制器)/Compose/View/WBComposeTextView.swift
+++ STTWeiBo/Classes/View(视图和控制器)/Compose/View/WBComposeTextView.swift
@@ -11,5 +11,41 @@ import UIKit
 /// 撰写微博的文本视图
 class WBComposeTextView: UITextView {
 
-  
+  // 占位标签
+    private lazy var placeholderLabel = UILabel()
+    
+    override func awakeFromNib() {
+        
+        setupUI()
+    }
+    
+    //MARK: - 监听方法
+    @objc private func textChange(n:NSNotification){
+        print(n)
+        //如果有文本 不显示占位标签 否则 显示
+        placeholderLabel.hidden = self.hasText()
+        
+    }
+    
+    deinit{
+        NSNotificationCenter.defaultCenter().removeObserver(self)
+    }
+}
+
+
+private extension WBComposeTextView{
+    
+    func setupUI(){
+        // 0 注册通知
+        NSNotificationCenter.defaultCenter().addObserver(self, selector: #selector(textChange), name: UITextViewTextDidChangeNotification, object: self)
+        
+        // 1 设置占位标签
+        placeholderLabel.text = "分享新鲜事..."
+        placeholderLabel.font = self.font
+        
+        placeholderLabel.sizeToFit()
+        placeholderLabel.textColor = UIColor.lightGrayColor()
+        placeholderLabel.frame.origin = CGPoint(x: 5, y: 8)
+        addSubview(placeholderLabel)
+    }
 }
