e6b1c987e6392891067cb680d3ff76cb8a32cc31
suntingting <suntt@wafa.com.cn>
2017-04-07 14:31:06 +0800

    确定加载微博函数的参数 和 完成回调  实现伪代码

--- STTWeiBo/Classes/ViewModel(视图模型)/WBStatusListDAL.swift
+++ STTWeiBo/Classes/ViewModel(视图模型)/WBStatusListDAL.swift
@@ -13,6 +13,20 @@ import Foundation
 /// 在调整系统的时候 尽量做最小化的调整
 class WBStatusListDAL {
     
-    
+    /**
+     从本地数据库 或者 网络加载数据
+     
+     - parameter since_id:   下拉刷新 id
+     - parameter max_id:     上拉刷新 id
+     - parameter completion: 完成回调（微博的字典数组 是否成功）
+     */
+    class func loadStatus(since_id:Int64 = 0,max_id:Int64 = 0,completion:(list:[[String:AnyObject]]?,isSuccess:Bool)->()){
+        
+        // 1 检查本地数据 如果有 直接返回
+        
+        // 2 加载网络数据
+        // 3 加载完成之后 将网络数据[字典数组] 写入数据库
+        // 4 返回网络数据
+    }
     
 }
