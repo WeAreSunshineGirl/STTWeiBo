9315b3852e7c10a4da633294f0e2f4ba74a04516
suntingting <suntt@wafa.com.cn>
2017-04-07 11:20:24 +0800

    新增数据访问层 确定数据缓存思路

---
+++ STTWeiBo/Classes/ViewModel(视图模型)/WBStatusListDAL.swift
@@ -0,0 +1,18 @@
+//
+//  WBStatusListDAL.swift
+//  STTWeiBo
+//
+//  Created by user on 17/4/7.
+//  Copyright © 2017年 user. All rights reserved.
+//
+
+import Foundation
+
+//DAL - Data Access Layer 数据访问层
+//使命:负责处理数据库和网络数据 给 ListViewModel 返回微博的[字典数组]
+/// 在调整系统的时候 尽量做最小化的调整
+class WBStatusListDAL {
+    
+    
+    
+}
